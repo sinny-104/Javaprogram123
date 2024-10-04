f1020ca188ffe4cee3a72a62b6adb040ddbe1187
HP <jajnasenee.baral1995@gmail.com>
2024-10-04 15:32:28 +0530

    my files

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,12 @@
+package Oct.ex_03102024;
+public class Lab002 {
+        public static void main(String[] args) {
+            System.out.println(20+3);
+            System.out.println(20-3);
+            System.out.println(20*3);
+            System.out.println(20/3);
+            System.out.println(10*3);
+
+        }
+    }
+
